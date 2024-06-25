870728021cd6a483fca918512f4b5e31c76630c0
Davit Mayilyan <davit.mayilyan@protonmail.ch>
2024-06-25 18:38:19 +0400

    Switch to lists instead of str.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from itertools import zip_longest
+from collections import deque
 from os import path
 from typing import Iterable, Tuple, List
 
 
-def split_files(paths: Iterable[str]) -> Tuple[List[str], Tuple[str]]:
+def split_paths(paths: Iterable[str]) -> Tuple[List[str], Tuple[str]]:
     split_blocks = [path.split(p) for p in paths]
 
     paths, files = zip(*((p, f) for p, f in split_blocks))
@@ -12,14 +14,59 @@ def split_files(paths: Iterable[str]) -> Tuple[List[str], Tuple[str]]:
 
 
 def create_final_list(paths) -> List[str]:
-    return len(paths) * [""]
+    return [[] for _ in paths]
 
 
-def create_box_list(paths):
-    splits = [i.split("/") for i in paths]
-    print(splits)
-    max_path_len = max(map(len, splits))
-    #  box_list = max_path_len
+def process_paths(paths, final_list):
+    for i, slice_list in enumerate(zip_longest(*paths, fillvalue="")):
+        if all(map(lambda x: x < 10, map(len, slice_list))):
+            for pathi, p in enumerate(slice_list):
+                final_list[pathi].append(p)
+            continue
+
+        max_in_slice = max(map(len, slice_list))
+        slice_list = [i.ljust(max_in_slice, chr(0)) for i in slice_list]
+        #  print(slice_list)
+
+        mask = []
+        for vals in zip(*slice_list):
+            if len(set(vals)) != 1:
+                mask.append(True)
+            else:
+                mask.append(False)
+
+        surround_size = 2
+
+        mask_shifted = mask.copy()
+        mask_shifted += [False] * surround_size
+        mask_shifted = mask_shifted[surround_size:]
+        final_mask = [a or b for a, b in zip(mask, mask_shifted)]
+
+        buffer_list = create_final_list(slice_list)
+        for vals in zip(*slice_list, final_mask):
+            if len(set(vals[:-1])) == 1 and not vals[-1]:
+                for vali in range(len(vals[:-1])):
+                    if not buffer_list[vali-1] == ".":
+                        buffer_list[vali].append(".")
+            else:
+                for vali in range(len(vals[:-1])):
+                    buffer_list[vali].append(vals[vali].strip(chr(0)))
+
+
+        buffer_list = ["".join(item) for item in buffer_list]
+
+        for bi, fi in zip(buffer_list, final_list):
+            fi.append(bi)
+
+        print("!!!", final_list)
+
+
+
+    print(final_list)
+    print("/".join(final_list[0]))
+
+
+
 
 #  splits = a.split("/"), b.split("/")
 
@@ -78,12 +125,15 @@ def create_box_list(paths):
 if __name__ == "__main__":
     a = "ns-client-bavo-protocol-manual-lhc-mellinbright-catmetrics/to/somewhere/far/far/away/foo.bar"
     b = "ns-client-bavo-task-script-lhc-plate-reader-echo-catmetrics/to/somewhere/far/far/away/notfoo.bar"
-    c = "ns-task-script-hello-world/Lab1"
+    #  c = "ns-task-script-hello-world/Lab1"
 
-    path_list = [a, b, c]
-    paths, filenames = split_files(path_list)
+    path_list = [a, b]
+    paths, filenames = split_paths(path_list)
+
+    # TODO
+    d = deque()
 
     final_list = create_final_list(paths)
     print(final_list)
 
-    #  create_box_list(path_list)
+    process_paths(paths, final_list)
--- test_main.py
+++ test_main.py
@@ -4,8 +4,10 @@ from pytest import fixture
 
 @fixture
 def path_list():
-    a = "ns-client-bavo-protocol-manual-lhc-mellinbright-catmetrics/to/somewhere/far/far/away/foo.bar"
-    b = "ns-client-bavo-task-script-lhc-plate-reader-echo-catmetrics/to/somewhere/far/far/away/notfoo.bar"
+    a = "ns-client-bavo-protocol-manual-lhc-mellinbright-catmetrics/to/" \
+        "somewhere/far/far/away/foo.bar"
+    b = "ns-client-bavo-task-script-lhc-plate-reader-echo-catmetrics/to/" \
+        "somewhere/far/far/away/notfoo.bar"
     c = "ns-task-script-hello-world/Lab1"
 
     return [a, b, c]
@@ -34,16 +36,16 @@ def path_only_list():
     return [a, b, c]
 
 
-def test_split_files(path_list, path_only_list):
+def test_split_paths(path_list, path_only_list):
     expected = (
         path_only_list,
         ("foo.bar", "notfoo.bar", "Lab1"),
     )
-    assert split_files(path_list) == expected
+    assert split_paths(path_list) == expected
 
 
 def test_create_final_paths(path_list):
-    expected = ["", "", ""]
+    expected = [[], [], []]
     assert create_final_list(path_list) == expected
 
 
